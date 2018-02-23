5b5641256fb506394af74d21d5c44a728046277b
Nick <rn3kk.nick@gmail.com>
2018-02-23 23:40:07 +0300

    -в конфиге разделены вых/вх пины -добавлена загрузка из конфига данных об пинах с которыми может работать железка (распберри) и которые мы мониторим

--- RemoteSwitcher/src/GPIOSwitcher.py
+++ RemoteSwitcher/src/GPIOSwitcher.py
@@ -1,9 +1,18 @@
 import json
 
-class Pin:
+from enum import Enum
+
+__metaclass__ = type
+
+class PinType(Enum):
+    input=1
+    output=2
+
+
+class Pin: #Base class for output pin
     __pinName = None
     __pinNumber = None
-    __pinState = None
+
     def __init__(self, pinName, pinNumber):
         self.__pinName = pinName
         self.__pinNumber = pinNumber
@@ -14,24 +23,43 @@ class Pin:
     def getPinNumber(self):
         return self.__pinNumber
 
-    def getPinState(self):
-        return self.__pinState
+
+class OutputPin(Pin):
+    __autoOffTime = 0
+    def __init__(self, pinName, pinNumber, autoOffTime = 0):
+        super(OutputPin, self).__init__(pinName, pinNumber)
+        self.__autoOffTime = autoOffTime
+
+
+
 
 class GPIOSwitcher:
-    __pinsList = None
+    __outputPinsList = None
+    __inputPinsList = None
 
     def __init__(self):
         print("Load pins from file ../res/pins")
-        self.__pinsList = list()
+        self.__outputPinsList = list()
+        self.__inputPinsList = list()
         file = open("../res/pins")
         data = file.read()
         file.close()
         jsonPins = json.loads(data)
-        pinsList = jsonPins["pins"]
-        for pinDict in pinsList:
-            name = pinDict.get("name")
-            number = pinDict.get("number")
-            self.__pinsList.append(Pin(name, number))
+
+        outputPinsList = jsonPins["pins_output"]
+        for pin in outputPinsList: #load output pins
+            name = pin.get("name")
+            gpio_number = pin.get("gpio_number")
+            autoOff = pin.get("auto_off")
+            if autoOff is None:
+                autoOff = 0
+            self.__outputPinsList.append(OutputPin(name, gpio_number, autoOff))
+
+        inputPinsList = jsonPins["pins_input"]
+        for pin in outputPinsList:  # load output pins
+            name = pin.get("name")
+            gpio_number = pin.get("gpio_number")
+            self.__outputPinsList.append(Pin(name, gpio_number))
 
     def getGpioState(self):
         return 0
