09fbba138efbb3e48c1dd08ab81c5117c624ee1b
Nick <rn3kk.nick@gmail.com>
2018-11-10 22:20:09 +0300

    В выходную информацию о пинах выводится тип пина (для дальнейшей группировки кнопок в под одну форму) Начата работа над авторизацией

--- RemoteSwitcher/src/GPIOController.py
+++ RemoteSwitcher/src/GPIOController.py
@@ -2,7 +2,7 @@ import json
 import time
 from enum import Enum
 
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 from threading import Thread
 
 __metaclass__ = type
@@ -15,6 +15,7 @@ GPIO_NUMBER = "gpioNumber"
 AUTO_OFF = "auto_off"
 STATE = "state"
 TYPE = "type"
+GROUP = "group"
 REQUEST = "request"
 GET_GPIO_STATE = "getGpioState"
 CHANGE_GPIO_STATE = "changeGpioState"
@@ -34,11 +35,14 @@ class Pin:  # Base class for output pin
     _pinNumber = None
     _pinState = None
     __pinType = None
+    __pinGroup=None
 
-    def __init__(self, pinName, pinNumber, pinType):
+
+    def __init__(self, pinName, pinNumber, pinType, pinGroup):
         self.__pinName = pinName
         self._pinNumber = pinNumber
         self.__pinType = pinType
+        self.__pinGroup = pinGroup
 
     def getPinName(self):
         return self.__pinName
@@ -55,18 +59,20 @@ class Pin:  # Base class for output pin
         if self.__pinType == PinType.OUTPUT:
             return 1
 
+    def getPinGroup(self):
+        return self.__pinGroup
 
 class InputPin(Pin):
 
-    def __init__(self, pinName, pinNumber):
+    def __init__(self, pinName, pinNumber, pinGroup):
        print('InputPin ', pinName, 'number ', pinNumber)
-       super(InputPin, self).__init__(pinName, pinNumber, PinType.INPUT)
-       GPIO.setmode(GPIO.BCM)
-       GPIO.setup(int(pinNumber), GPIO.IN)
+       super(InputPin, self).__init__(pinName, pinNumber, PinType.INPUT, pinGroup)
+ #      GPIO.setmode(GPIO.BCM)
+ #      GPIO.setup(int(pinNumber), GPIO.IN)
 
     def updatePinState(self):
         self._pinState = 1
-        self._pinState = GPIO.input(int(self._pinNumber))
+ #       self._pinState = GPIO.input(int(self._pinNumber))
 
 class OutputPin(Pin):
 
@@ -74,12 +80,10 @@ class OutputPin(Pin):
     __timeLeft = 0
     __defaultState = 0;
 
-    def __init__(self, pinName, pinNumber, autoOffTime=0, pinState=False):
+    def __init__(self, pinName, pinNumber, pinGroup, autoOffTime=0, pinState=False):
         print('OutputPin ', pinName, 'number ', pinNumber, "autoOffTime", autoOffTime)
-        super(OutputPin, self).__init__(pinName, pinNumber, PinType.OUTPUT)
+        super(OutputPin, self).__init__(pinName, pinNumber, PinType.OUTPUT, pinGroup)
         self.__autoOffTime = autoOffTime
-        #GPIO.setmode(GPIO.BCM)
-        #GPIO.setup(int(pinNumber), GPIO.OUT)
         self.__setState(pinState)
         self.__defaultState = pinState
 
@@ -91,9 +95,9 @@ class OutputPin(Pin):
     def __setState(self, state):
         self._pinState = state
         try:
-            GPIO.setmode(GPIO.BCM)
-            GPIO.setup(int(self._pinNumber), GPIO.OUT)
-            GPIO.output(int(self._pinNumber), bool(state))
+            #GPIO.setmode(GPIO.BCM)
+            #GPIO.setup(int(self._pinNumber), GPIO.OUT)
+            #GPIO.output(int(self._pinNumber), bool(state))
             print('New statete for OutputPin ', self.getPinName(), 'number ', self.getPinNumber(), 'sate ', self.getPinState() )
         except Exception:
             print('Error: output pin state NOT changed')
@@ -148,17 +152,18 @@ class GPIOController(Thread):
                 autoOff = 0
             state = pin.get(STATE)
             type = pin.get(TYPE)
+
             if type == "bmz":
                 if name == "bit1":
-                    bmzPin0 = OutputPin(name, gpio_number, autoOff, state)
+                    bmzPin0 = OutputPin(name, gpio_number, type, autoOff, state)
                 if name == "bit2":
-                    bmzPin1 = OutputPin(name, gpio_number, autoOff, state)
+                    bmzPin1 = OutputPin(name, gpio_number, type, autoOff, state)
                 if name == "bit3":
-                    bmzPin2 = OutputPin(name, gpio_number, autoOff, state)
+                    bmzPin2 = OutputPin(name, gpio_number, type, autoOff, state)
                 if name == "bit4":
-                    bmzPin3 = OutputPin(name, gpio_number, autoOff, state)
+                    bmzPin3 = OutputPin(name, gpio_number, type, autoOff, state)
             else:
-                self.__outputPinsList.append(OutputPin(name, gpio_number, autoOff, state))
+                self.__outputPinsList.append(OutputPin(name, gpio_number, type, autoOff, state))
 
         if not (bmzPin0 is None) and not (bmzPin1 is None) and not (bmzPin2 is None) and not (bmzPin3 is None):
             self.__bmz = Bmz(bmzPin0, bmzPin1, bmzPin2, bmzPin3)
@@ -168,7 +173,7 @@ class GPIOController(Thread):
             name = pin.get(NAME)
             gpio_number = pin.get(GPIO_NUMBER)
             type = pin.get(TYPE)
-            self.__inputPinsList.append(InputPin(name, gpio_number))
+            self.__inputPinsList.append(InputPin(name, gpio_number, type))
 
     def run(self):
         timeSleepInterval = 1
@@ -183,11 +188,12 @@ class GPIOController(Thread):
     def getGpioState(self):
         input, output = [], []
         for outputPin in self.__outputPinsList:
-            str = {NAME: outputPin.getPinName(), STATE: outputPin.getPinState(), TYPE: outputPin.getPinType()}
+            str = {NAME: outputPin.getPinName(), STATE: outputPin.getPinState(), TYPE: outputPin.getPinType(), GROUP: outputPin.getPinGroup() }
             output.append(str)
-        output.append(self.__bmz.toOutputPin())
+        if self.__bmz <> None:
+            output.append(self.__bmz.toOutputPin())
         for inputPin in self.__inputPinsList:
-            str = {NAME: inputPin.getPinName(), STATE: inputPin.getPinState(), TYPE: inputPin.getPinType()}
+            str = {NAME: inputPin.getPinName(), STATE: inputPin.getPinState(), TYPE: inputPin.getPinType(), GROUP: inputPin.getPinGroup() }
             input.append(str)
         response = {}
         response[TYPE] = GET_GPIO_STATE
@@ -256,5 +262,5 @@ class Bmz:
         bmzNum = (bmzNum << 1) | int(self.__pin2.getPinState())
         bmzNum = (bmzNum << 1) | int(self.__pin3.getPinState())
 
-        str = {NAME: BMZ, STATE: bmzNum, TYPE: 1}
+        str = {NAME: BMZ, STATE: bmzNum, TYPE: 1, GROUP: (self.__pin0).getPinGroup()}
         return str
