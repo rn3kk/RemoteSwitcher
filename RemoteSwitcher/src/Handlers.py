1d3d0a95d3abb0bca67ad69de3eefcacac5fc025
Nick <rn3kk.nick@gmail.com>
2019-05-09 16:02:13 +0300

    Сделана basic авторизация - работает

--- RemoteSwitcher/src/Handlers.py
+++ RemoteSwitcher/src/Handlers.py
@@ -2,6 +2,8 @@ from threading import Thread, Lock
 from GPIOController import GPIOController, REQUEST, GET_GPIO_STATE, CHANGE_GPIO_STATE, NAME, CHANGE_BMZ, BMZ_NUMBER
 import socket
 import json
+import base64
+from Users import Users
 
 import eventlet
 from eventlet import wsgi
@@ -18,6 +20,8 @@ class RequestType:
     REQUEST_GET_PIN_STATE = 2
     REQUEST_SET_PIN_STATE = 3
     REQUEST_LOGIN = 4
+    REQUEST_LOGIN_PAGE = 5
+    AUTORISATION = 6
 
 
 SEC_WEBSOCKET_KEY = "Sec-WebSocket-Key:"
@@ -25,17 +29,26 @@ SEC_KEY_SUFFIX = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
 SEC_WEBSOCKET_EXTENSION = "Sec-WebSocket-Extensions"
 HTTP_RESPONSE = 'HTTP/1.1 200 OK\r\nCache-Control : no-cache, private\r\nContent-Type: text/html\r\nContent-Length : %d\r\nDate : Mon, 24 Nov 2014 12:03:43 GMT\r\n\r\n'
 HTTP_RESPONSE2 = 'HTTP/1.1 200 OK\r\nDate : Mon, 24 Nov 2014 12:03:43 GMT\r\nCache-Control : no-cache, private\r\nContent-Length : %d\r\nContent-Type: text/html\r\n'
+HTTP_RESPONSE_UNAUTORISE = 'HTTP/1.1 401 Authorization Required\r\nDate: Tue, 01 Mar 2005 11:30:10 GMT\r\nServer: Apache/1.3.33 (Unix)\r\nWWW-Authenticate: Basic realm="How about authorization?"\r\nConnection: close\r\nContent-Type: text/html; charset=iso-8859-1"\r\n\r\n'
+HTTP_BA_TAG = 'Authorization: Basic '
+
+LOGIN_REQ = "login="
+
+LOGIN = "rn3kk:admin"
 
 class HttpRequestHandler(Thread):
-
+    __users = None
     def __init__(self):
         Thread.__init__(self)
 
     def run(self):
+        self.__users = Users()
+        port = 81
         sock = socket.socket()
-        sock.bind(('', 81))
+        sock.bind(('', port))
         sock.listen(1)
-        print 'waite connection to port 81'
+        print 'waite connection to port '
+        print port
 
         while True:
             conn, addr = sock.accept()
@@ -44,15 +57,26 @@ class HttpRequestHandler(Thread):
             if not req:
                 print("request is empty")
                 break
-            requestType = self.__getRequestType(req)
-            if(requestType == RequestType.REQUEST_LOGIN):
-                #conn.send(self.getIndexPage())
-                conn.send(self.getLoginResult())
-            elif(requestType == RequestType.REQUEST_GET_INDEX_PAGE):
+            if not self.checkAutorisation(req):
                 conn.send(self.getLoginPage())
+            else:
+                requestType = self.__getRequestType(req)
+                if requestType == RequestType.REQUEST_GET_INDEX_PAGE:
+                    conn.send(self.getIndexPage())
             conn.close()
 
         conn.close()
+
+    def checkAutorisation(self, req):
+        pos = req.find(HTTP_BA_TAG)
+
+        if pos == -1:
+            return False
+        pairBase64 = req[pos+len(HTTP_BA_TAG):]
+        endPair = pairBase64.find('\r\n')
+        pairBase64 = pairBase64[:endPair]
+        pair = base64.b64decode(pairBase64).decode('utf-8')
+        return self.__users.checkUser(pair)
 
     def getIndexPage(self):
         file = open("../res/index.html")
@@ -64,29 +88,11 @@ class HttpRequestHandler(Thread):
         return page
 
     def getLoginPage(self):
-        file = open("../res/login.html")
-        data = file.read()
-        file.close()
-        content_length = len(data)
-        page = (HTTP_RESPONSE % content_length)
-        page = page + data
-        return page
-
-#TODO тут нужно вернуть токен в форму и на форме сделат редирект на страницу index. Туда передать токен который будет использоваться для авторизации
-    def getLoginResult(self):
-        data = "login is ok all ok"
-        content_length = len(data)
-        page = (HTTP_RESPONSE % content_length)
-        page = page + data
-        return page
+        return HTTP_RESPONSE_UNAUTORISE
 
     def __getRequestType(self, request):
-        if "GET" in request:
-            return RequestType.REQUEST_GET_INDEX_PAGE;
-        if "POST /login" in request:
-            return RequestType.REQUEST_LOGIN
-
-
+        if "GET /" in request:
+            return RequestType.REQUEST_GET_INDEX_PAGE
 
 #mutex = Lock()
 def sendToWs(ws, text):
@@ -135,6 +141,6 @@ class WebSocketHandler(Thread):
         Thread.__init__(self)
 
     def run(self):
-        listener = eventlet.listen(('0.0.0.0', 8080))
+        listener = eventlet.listen(('0.0.0.0', 8081))
         print("\nVisit http://localhost:8080/ in your websocket-capable browser.\n")
         wsgi.server(listener, dispatch)
---
+++ RemoteSwitcher/src/Users.py
@@ -0,0 +1,18 @@
+class Users:
+    __userList = None
+
+    def __init__(self):
+        self.__userList = []
+        with open('../res/login') as f:
+            for line in f:
+                line = line.rstrip("\n")
+                if len(line) > 2 :
+                    self.__userList.append(line)
+
+    def checkUser(self, pair):
+        result = False
+        for el in self.__userList:
+            if el == pair:
+                result = True
+                break
+        return result
